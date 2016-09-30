99cff6b4aefc19778c28c81fcd7af58996c39576
Caitlin Stein <caitlin.teter@gmail.com>
2016-09-30 14:07:32 -0500

    pulled routes into their own file routes/index.js

---
+++ server/routes/index.js
@@ -0,0 +1,62 @@
+'use strict'
+
+const { Router } = require('express')
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+const Trophy = require('../models/trophy')
+
+const router = Router()
+
+router.post('/register', ({body: {email, password}}, res) => {
+      return new Promise((resolve, reject) => {
+      bcrypt.hash(password, 15, (err, hash) => {
+              if (err) {
+                reject(err)
+              } else {
+                resolve(hash)
+              }
+            })
+          })
+      .then(hash =>  {
+        res.send({msg:"User successfully created"})
+        return User.create({ email, password: hash })
+      })
+      //.then(() => res.redirect('/login'))
+      .catch(console.error)
+})
+
+router.post('/login', ({ session, body: { email, password } }, res, err) => {
+  User.findOne({ email })
+     .then(user => {
+       if (user) {
+         return new Promise((resolve, reject) =>
+           bcrypt.compare(password, user.password, (err, matches) => {
+             if (err) {
+               reject(err)
+             } else {
+               resolve(matches)
+             }
+           })
+         )
+       } else {
+         res.render('login', { msg: 'Email does not exist in our system' })
+       }
+     })
+     .then((matches) => {
+       if (matches) {
+         session.email = email
+         res.redirect('/')
+       } else {
+         res.render('login', { msg: 'Password does not match' })
+       }
+      })
+})
+
+router.get('/api/trophies', (req, res, err) => {
+  Trophy
+  .find()
+  .then(trophies => res.json({trophies}))
+  .catch(err)
+})
+
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -2,14 +2,12 @@
 
 const { json } = require('body-parser')
 const express = require('express')
-//const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const session = require('express-session')
 const RedisStore = require('connect-redis')(session)
 
-const User = require('./models/user')
-const Trophy = require('./models/trophy')
 const { connect } = require('./database/database')
+const routes = require('./routes/')
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -31,57 +29,7 @@ app.use((req, res, next) => {
   next()
 })
 
-app.post('/login', ({ session, body: { email, password } }, res, err) => {
-  User.findOne({ email })
-     .then(user => {
-       if (user) {
-         return new Promise((resolve, reject) =>
-           bcrypt.compare(password, user.password, (err, matches) => {
-             if (err) {
-               reject(err)
-             } else {
-               resolve(matches)
-             }
-           })
-         )
-       } else {
-         res.render('login', { msg: 'Email does not exist in our system' })
-       }
-     })
-     .then((matches) => {
-       if (matches) {
-         session.email = email
-         res.redirect('/')
-       } else {
-         res.render('login', { msg: 'Password does not match' })
-       }
-      })
-})
-
-app.post('/register', ({body: {email, password}}, res) => {
-      return new Promise((resolve, reject) => {
-      bcrypt.hash(password, 15, (err, hash) => {
-              if (err) {
-                reject(err)
-              } else {
-                resolve(hash)
-              }
-            })
-          })
-      .then(hash =>  {
-        res.send({msg:"User successfully created"})
-        return User.create({ email, password: hash })
-      })
-      //.then(() => res.redirect('/login'))
-      .catch(console.error)
-})
-
-app.get('/api/trophies', (req, res, err) => {
-  Trophy
-  .find()
-  .then(trophies => res.json({trophies}))
-  .catch(err)
-})
+app.use(routes)
 
 //listen
 connect()
