73ca9a71c6b85cee0cc82070327efee04301ecfd
Caitlin Stein <caitlin.teter@gmail.com>
2016-10-10 10:14:26 -0500

    can capture user ID and trophy id on trophy click

--- client/app/controllers/trophies.js
+++ client/app/controllers/trophies.js
@@ -1,5 +1,5 @@
-app.controller("TrophiesCtrl", ["$scope", "$http",
-  function($scope, $http) {
+app.controller("TrophiesCtrl", ["$scope", "$http", "$sessionStorage",
+  function($scope, $http, $sessionStorage) {
      
 $scope.fun = "trophies are fun"
 
@@ -7,6 +7,17 @@ $http.get('/api/trophies')
   .then( ({data: {trophies}}) => 
     $scope.trophies = trophies
     )
+
+if ($sessionStorage.currentUser) {
+  $scope.loggedInUser = $sessionStorage.currentUser
+  $scope.userId = $scope.loggedInUser._id
+}
+
+$scope.claimTrophy = (id) => {
+  console.log("trophy claimed");
+  console.log("user id", $scope.loggedInUser._id);
+  console.log("trophy id!", id);
+}
 
 
 }]);
