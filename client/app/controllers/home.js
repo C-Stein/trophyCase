02c01f0c3d1549d0afdf940e0a2fb0ccd762b1b0
Caitlin Stein <caitlin.teter@gmail.com>
2016-11-08 10:08:03 -0600

    displaying trophies by group on home page in progress

--- client/app/controllers/home.js
+++ client/app/controllers/home.js
@@ -14,9 +14,9 @@ if ($sessionStorage.currentUser) {
 if(!$sessionStorage.userTrophies){
   $http.get(`/api/userTrophies/${$scope.userId}`)
     .then( ({data}) => {
-      console.log("data, ", data);
+      //console.log("data, ", data);
       $sessionStorage.userTrophies = data.trophies
-      console.log("$sessionStorage.userTrophies", $sessionStorage.userTrophies)
+      //console.log("$sessionStorage.userTrophies", $sessionStorage.userTrophies)
       $scope.trophies = data.trophies
     })
 } else {
@@ -32,6 +32,19 @@ if(!$sessionStorage.userGroups){
     })
 } else {
   $scope.groups = $sessionStorage.userGroups
+}
+
+//for each trophy in each group, replace the -id, with the trophy object
+console.log("$scope.groups", $scope.groups)
+for (var i = 0; i < $scope.groups.length; i++) {
+  console.log("$scope.groups[i].groupTrophies", $scope.groups[i].groupTrophies)
+  console.log("$scope.trophies", $scope.trophies);
+//if  group trophy is in userTrophies, replace
+// if($scope.trophies //includes $scope.groups[i].groupTrohies) {
+  // replace $scope.groups[i].groupTrophies with actual trophy object
+// } else {
+     //remove from the array
+//}
 }
 
 }]);
