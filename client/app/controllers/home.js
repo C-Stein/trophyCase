14d1db6f46bb779cf32756bb0c1e8763f9aac17f
Caitlin Stein <caitlin.teter@gmail.com>
2017-01-19 12:04:36 -0600

    removed empty obj from .delete

--- client/app/controllers/home.js
+++ client/app/controllers/home.js
@@ -49,8 +49,7 @@ putTrophyObjInGroups = () => {
   //removing a trophy from a user
   $scope.removeTrophy = (id) => {
     $http.delete(`/api/users`,
-                    {},
-                    { params: {trophyId: id, userId: $scope.userId}})
+                    { params: {trophyId: id, userId: $scope.userId }})
       .then((data) => {
         console.log('data after removing trophy', data);
         delete $sessionStorage.userTrophies;
@@ -58,5 +57,5 @@ putTrophyObjInGroups = () => {
       })
   }
 
-
+console.log('$sessionStorage', $sessionStorage)
 }]);
